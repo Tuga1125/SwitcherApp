a0c3fde49a9154ffa1c82f29bff248426615b8b5
Tooga Garanja Magar <tugamagar77@gmail.com>
2023-10-04 16:46:28 -0400

    Initial Commit

---
+++ SwitcherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwitcherApp
+//
+//  Created by Tooga Garanja Magar on 2023-10-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
